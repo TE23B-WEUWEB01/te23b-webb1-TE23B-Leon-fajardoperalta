29d27f35cacc34579c9a21ec3475e49b4c66e08d
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2025-03-13 11:06:33 +0100

    kankatenering träna js variables

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,14 @@
+
+// var ålder = 99;
+// var lol = 1;
+// var summa = lol + ålder;
+// alert(ålder + "+" + lol + "=" + summa);
+
+var fNamn = "lee";
+var efNamn = "hon";
+var efNamn2 = "jon";
+var namn = fNamn + " " + efNamn + " " + efNamn2;
+var hobby = "musik";
+var fweage = 9352532;
+var mening = namn+ "´s" + " " + "hobby är" + " " + hobby +" "+ ". \ndet kostar" +" " + "gewagewgewag" +" " +fweage
+alert(mening);
