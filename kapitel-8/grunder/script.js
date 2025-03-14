63d06b36198a139f6e29ef2ae84b5aca1be8b08f
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2025-03-14 14:04:54 +0100

    tränat på promt alert var (100 - number(num3)) / 100

---
+++ kapitel-8/grunder/script.js
@@ -0,0 +1,59 @@
+// var dag = "fredag";
+
+
+// alert(dag);
+
+// console.log("idag " + dag);
+
+
+// var stad = prompt("Var är du född?");
+
+// console.log("du är född i " + stad);
+
+
+
+//  vi gör en enkel captcha
+
+// var gissning = prompt("är det ok att fuska på prov?");
+// if(gissning == "nej")
+// {
+//     alert("du är människa");
+// } else
+// {
+//     alert("what the hell");
+// }
+
+// var slumptal1 = Math.round(Math.random() * 100);
+// var slumptal233 = Math.round(Math.random() * 100);
+
+
+// var froga = prompt("vad är " + slumptal1 +" + "+ slumptal233 );''
+
+// if(froga == slumptal1+slumptal233)
+// {
+//     document.writeln("<h1> bra jobbat </h1>");
+// }else
+// {
+//     alert("hmph");
+// }
+
+// document.writeln("<h1> slumptal1 = " + slumptal1 + "</h1>");
+
+// var num1 = prompt("vad är" );
+// var num2 = prompt("plus");
+
+
+
+// var summa = Number(num1)+Number(num2);
+// alert( "och det blir " + summa);
+
+
+//  skatteuträkning
+
+
+var num4 = prompt("hur mycket peng fo du " );
+var num3 = prompt("vad din skatt? ");
+
+var sum = Number(num4) * (100 - Number(num3)) / 100;
+
+alert(sum);
--- kapitel-8/intro/script.js
+++ kapitel-8/intro/script.js
@@ -8,7 +8,12 @@ var fNamn = "lee";
 var efNamn = "hon";
 var efNamn2 = "jon";
 var namn = fNamn + " " + efNamn + " " + efNamn2;
+
+// hobby
 var hobby = "musik";
 var fweage = 9352532;
 var mening = namn+ "´s" + " " + "hobby är" + " " + hobby +" "+ ". \ndet kostar" +" " + "gewagewgewag" +" " +fweage
+
+
+// skriv ut
 alert(mening);
