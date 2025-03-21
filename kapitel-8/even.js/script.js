45246d9f1e824cac2c93b6fcd528fda8455cd4cd
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2025-03-21 14:09:37 +0100

    omg de funkar if loopar

---
+++ kapitel-8/even.js/script.js
@@ -0,0 +1,29 @@
+
+// while(true){
+//     let namn = prompt("ange ditt namn:")
+//     let los = prompt("ange ditt losen")
+
+//     if(namn == "lol" && los == "lol2" )
+//     { 
+//         document.writeln("wow");
+//         break;
+//     }
+//     else
+//     {
+//         document.writeln("wow");
+//     }
+// }
+
+// let ålder = prompt("hur gam ar du");
+
+// document.writeln(` hej alex du är ${ålder} `);
+
+
+// läs in ett heltal mellan 10 och 50 
+
+let lol = parseInt(prompt("skriv ett heltal från 5 till 10:"))
+
+for(let i = 1; i <= lol; i++)
+{
+    document.writeln(`rad ${i}<br>`);
+}
