84ade877b9cd1551c600392e9ec078632723e257
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2025-03-27 11:07:28 +0100

    query selector function  document .body

---
+++ kapitel-8/interaktive/script.js
@@ -0,0 +1,16 @@
+function DarkPage() {
+      document.body.style = "background: #000; color : #FFF;";
+}
+function WhitePage() {
+
+      document.body.style = "background: #FFF; color: #000;";
+      document.querySelector("h1").style = "font-size: 50px "
+}
+function size() {
+    document.querySelector("h1").style = "font-size: 100px ; border-bottom: 10px solid black ;   text-shadow: 0 0 5px black;"
+
+}
+
+function red() {
+    document.querySelector("p").style = " visibility: visible;"
+}
